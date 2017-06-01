608d74698b3209a99df9119d3ce1c64b62e517c9
Pete Slater <pslater@equalexperts.com>
2017-06-01 14:43:50 +0100

    APIS-1728 - Added test for marriage allowance eligibility

--- app/controllers/MarriageAllowanceEligibility.scala
+++ app/controllers/MarriageAllowanceEligibility.scala
@@ -8,7 +8,7 @@ import uk.gov.hmrc.play.microservice.controller.BaseController
 import scala.concurrent.Future
 
 class MarriageAllowanceEligibility extends BaseController with StubResource {
-  def fetch(utr: String, nino: String, firstName: String, dateOfBirth: String, taxYear: String) = Action.async {
+  def fetch(utr: String, nino: String, firstname: String, surname: String, dateOfBirth: String, taxYear: String) = Action.async {
     implicit request =>
       val dataSetPath = "/resources/marriage-allowance-eligibility/happy_path.json"
       Future(jsonResourceAsResponse(dataSetPath))
--- test/controllers/MarriageAllowanceEligibilitySpec.scala
+++ test/controllers/MarriageAllowanceEligibilitySpec.scala
@@ -2,16 +2,32 @@ package controllers
 
 import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play.OneAppPerSuite
+import play.api.http.Status
+import play.api.libs.json.Json
 import play.api.test.FakeRequest
 import uk.gov.hmrc.play.filters.MicroserviceFilterSupport
 import uk.gov.hmrc.play.http.HeaderCarrier
 import uk.gov.hmrc.play.test.UnitSpec
+import util.ResourceLoader
 
-class MarriageAllowanceEligibilitySpec extends UnitSpec with MockitoSugar with OneAppPerSuite {
+class MarriageAllowanceEligibilitySpec extends UnitSpec with MockitoSugar with OneAppPerSuite with ResourceLoader {
   trait Setup extends MicroserviceFilterSupport {
     val request = FakeRequest().withHeaders("Accept" -> "application/vnd.hmrc.1.0+json")
     implicit val headerCarrier = HeaderCarrier()
 
     val underTest = new MarriageAllowanceEligibility()
+  }
+
+  "fetch" should {
+
+    "return the happy path response when called with a utr, nino, firstname, surname, dateOfBirth and taxYear" in new Setup {
+
+      val expected = loadResource("/resources/marriage-allowance-eligibility/happy_path.json")
+
+      val result = await(underTest.fetch("2234567890", "AA000003D", "John", "Smith", "1981-01-31", "2014-15")(request))
+
+      status(result) shouldBe Status.OK
+      jsonBodyOf(result) shouldBe Json.parse(expected)
+    }
   }
 }
