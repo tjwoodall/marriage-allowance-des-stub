98b77b9a9d27a7106683395ec50bcd5a26903b2e
danewatts <danewatts97@gmail.com>
2020-10-06 14:27:48 +0100

    Updated licenses and moved to build.sbt

---
+++ project/AppDependencies.scala
@@ -0,0 +1,32 @@
+import sbt._
+import play.sbt.PlayImport.ws
+
+object AppDependencies {
+
+  val hmrcTestVersion = "3.5.0-play-25" //TODO remove
+  val scalaTestPlusPlayVersion = "2.0.1"
+
+  lazy val compile: Seq[ModuleID] = Seq(
+    ws,
+    "uk.gov.hmrc" %% "microservice-bootstrap" % "10.4.0",
+    "uk.gov.hmrc" %% "domain" % "5.3.0",
+    "uk.gov.hmrc" %% "play-reactivemongo" % "6.4.0",
+    "uk.gov.hmrc" %% "play-hmrc-api" % "3.4.0-play-25"
+  )
+
+  lazy val test: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion,
+    "uk.gov.hmrc" %% "reactivemongo-test" % "3.1.0",
+    "org.scalatestplus.play" %% "scalatestplus-play" % scalaTestPlusPlayVersion,
+    "org.mockito" % "mockito-core" % "2.24.5"
+  ).map(_ % "test")
+
+  val itTest: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion,
+    "org.scalatestplus.play" %% "scalatestplus-play" % scalaTestPlusPlayVersion,
+    "org.scalaj" %% "scalaj-http" % "2.4.1",
+    "com.github.tomakehurst" % "wiremock" % "2.21.0"
+  ).map(_ % "it") //TODO refactor
+
+  val all: Seq[ModuleID] = compile ++ test ++ itTest
+}
