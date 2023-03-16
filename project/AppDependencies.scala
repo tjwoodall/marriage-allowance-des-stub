8c7360d91b87c6c112ab75861002512b1c71283e
Tateni93 <33066342+Tateni93@users.noreply.github.com>
2023-03-16 14:00:51 +0000

    DDCNL-6850: made PR builder changes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,10 +7,11 @@ object AppDependencies {
   val scalatestplusVersion = "5.1.0"
   val flexmarkallVersion = "0.62.2"
   val hmrcMongoVersion = "1.1.0"
+  val bootstrapVersion = "7.14.0"
 
   lazy val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"          %% "bootstrap-backend-play-28" % "7.14.0",
+    "uk.gov.hmrc"          %% "bootstrap-backend-play-28" % bootstrapVersion,
     "uk.gov.hmrc"          %% "domain"                    % "8.1.0-play-28",
     "uk.gov.hmrc.mongo"    %% "hmrc-mongo-play-28"        % hmrcMongoVersion
   )
@@ -20,7 +21,8 @@ object AppDependencies {
     "org.scalatestplus.play" %% "scalatestplus-play"      % scalatestplusVersion,
     "org.mockito"             % "mockito-core"            % "4.8.1",
     "com.vladsch.flexmark"    % "flexmark-all"            % flexmarkallVersion,
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % hmrcMongoVersion
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % hmrcMongoVersion,
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootstrapVersion,
   ).map(_ % "test")
 
   val itTest: Seq[ModuleID] = Seq(
@@ -29,7 +31,8 @@ object AppDependencies {
     "org.scalaj"                   %% "scalaj-http"          % "2.4.2",
     "com.github.tomakehurst"        % "wiremock-jre8"        % "2.34.0",
     "com.vladsch.flexmark"          % "flexmark-all"         % flexmarkallVersion,
-    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.13.4"
+    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.13.4",
+    "uk.gov.hmrc"                  %% "bootstrap-test-play-28" % bootstrapVersion,
   ).map(_ % "it")
 
   val all: Seq[ModuleID] = compile ++ test ++ itTest
