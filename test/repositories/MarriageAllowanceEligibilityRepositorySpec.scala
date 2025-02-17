a67c34661ecf02a84a0ea4ef92174f9a204b3fac
jmsatheesh <19515164+jmsatheesh@users.noreply.github.com>
2025-02-17 11:18:17 +0000

    scala 3 upgrade and compiler error fixes

--- test/repositories/MarriageAllowanceEligibilityRepositorySpec.scala
+++ test/repositories/MarriageAllowanceEligibilityRepositorySpec.scala
@@ -17,7 +17,7 @@
 package repositories
 
 import models.EligibilitySummary
-import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+import org.scalatest.matchers.should.Matchers._
 import org.scalatestplus.play.PlaySpec
 import play.api.test.Helpers.{await, defaultAwaitTimeout}
 import uk.gov.hmrc.mongo.test.DefaultPlayMongoRepositorySupport
@@ -25,7 +25,7 @@ import uk.gov.hmrc.mongo.test.DefaultPlayMongoRepositorySupport
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class MarriageAllowanceEligibilityRepositorySpec extends PlaySpec with DefaultPlayMongoRepositorySupport[EligibilitySummary] {
-  override lazy val repository = new MarriageAllowanceEligibilityRepository(mongoComponent)
+  override val repository:MarriageAllowanceEligibilityRepository = new MarriageAllowanceEligibilityRepository(mongoComponent)
 
   "MarriageAllowanceEligibilityRepository" must {
     "return an eligibility summary" when {
@@ -33,18 +33,18 @@ class MarriageAllowanceEligibilityRepositorySpec extends PlaySpec with DefaultPl
         val eligibilitySummary = EligibilitySummary("nino", "2021-22", "john", "smith", "19-03-75", true)
         val storeResult = await(repository.store(eligibilitySummary))
 
-        storeResult shouldBe eligibilitySummary
+        storeResult.shouldBe (eligibilitySummary)
         val fetchResult = await(repository.fetch("nino", "2021-22"))
 
-        fetchResult.isEmpty shouldBe false
-        fetchResult.get shouldBe eligibilitySummary
+        fetchResult.isEmpty.shouldBe(false)
+        fetchResult.get.shouldBe (eligibilitySummary)
       }
     }
     "return nothing" when {
       "there is no status summary that matches in the database" in {
         val result = await(repository.fetch("nino", "2021-22"))
 
-        result.isEmpty shouldBe true
+        result.isEmpty.shouldBe (true)
       }
     }
   }
